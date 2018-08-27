7a79500f8e57a78abc75ec8b27e7ffcaae9fb7a3
Reija Jokinen <reija.jokinen@kolumbus.fi>
2018-08-27 17:37:37 +0300

    Poistettu ylimääräisiä fileitä

--- src/main/java/fi/academy/controllers/UserController.java
+++ src/main/java/fi/academy/controllers/UserController.java
@@ -14,7 +14,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
--- src/main/java/fi/academy/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package fi.academy.repositories;
-
-import fi.academy.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    User findByUsername(String username);
-}
