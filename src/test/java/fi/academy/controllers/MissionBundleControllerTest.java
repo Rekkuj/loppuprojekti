572e9c3b68282a6ac802639a1ac1a97f2bbfc920
Reija Jokinen <reija.jokinen@kolumbus.fi>
2018-09-05 09:40:01 +0300

    MissionBundles testien viilausta

--- src/test/java/fi/academy/controllers/MissionBundleControllerTest.java
+++ src/test/java/fi/academy/controllers/MissionBundleControllerTest.java
@@ -44,14 +44,14 @@ public class MissionBundleControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
     
-    @Before
-    public void beforeEachTest() {
-        this.restTemplate = restTemplate;
-        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<ClientHttpRequestInterceptor>();
-        interceptors.add(new HeaderRequestInterceptor("Authorization", "Bearer " + accessToken.getAccess_token()));
-    
-        restTemplate.getRestTemplate().setInterceptors(interceptors);
-    }
+//    @Before
+//    public void beforeEachTest() {
+//        this.restTemplate = restTemplate;
+//        List<ClientHttpRequestInterceptor> interceptors = new ArrayList<ClientHttpRequestInterceptor>();
+//        interceptors.add(new HeaderRequestInterceptor("Authorization", "Bearer " + accessToken.getAccess_token()));
+//
+//        restTemplate.getRestTemplate().setInterceptors(interceptors);
+//    }
     
     HttpHeaders headers = new HttpHeaders();
     
@@ -61,107 +61,107 @@ public class MissionBundleControllerTest {
     
     /*Check that missionbundles are found*/
     @Test
-    public void getJermuUser(){
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-        
+    public void getJermujututMissionBundle() throws JSONException {
         response = this.restTemplate.getForObject("/missionbundles", String.class);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(urlWithPort("/missionbundles"), HttpMethod.GET, entity, String.class);
-        actual = responseEntity.getBody().toString();
-        assertTrue(actual.contains("{\"id\":1,\"username\":\"Jermu\",\"role\":\"CHIEF\",\"points\":2000,\"groupid\":1,\"completedmissions\":[\"Himmeli\",\"Helpperi\"],\"contactpersonuserid\":1,\"authid\":\"auth0|5b87943afe13090f5ffd652b\"}"));
+        JSONAssert.assertEquals("[{\n" +
+                "        \"bundleid\": 1,\n" +
+                "        \"listofmissions\": [\n" +
+                "            25,\n" +
+                "            3\n" +
+                "        ],\n" +
+                "        \"bundlename\": \"Kettujutut\"\n" +
+                "    },    {\n" +
+                "        \"bundleid\": 2,\n" +
+                "        \"listofmissions\": [\n" +
+                "            1,\n" +
+                "            2,\n" +
+                "            3\n" +
+                "        ],\n" +
+                "        \"bundlename\": \"Jermujutut\"\n" +
+                "    }]", response, false);
+    
     }
     
     /*Check that context loads and responsebody contains user "Jermu"*/
     @Test
-    public void getAllUsersTest(){
-        ResponseEntity<List<User>> responseEntity = restTemplate.exchange(urlWithPort("/missionbundles"), HttpMethod.GET, null, new ParameterizedTypeReference<List<User>>() {
+    public void getAllMissionBundlesTest(){
+        ResponseEntity<List<MissionBundle>> responseEntity = restTemplate.exchange(urlWithPort("/missionbundles"), HttpMethod.GET, null, new ParameterizedTypeReference<List<MissionBundle>>() {
         });
-        List<User> user = responseEntity.getBody();
-//        Integer lkm = jdbc.queryForObject("select count(*) from users", Integer.class);
-//        System.out.println("\n***** USER LKM " + lkm);
-        assertTrue(user.size()==6);
-    }
-    
-    /* Call GET by username */
-    @Test
-    public void getOneUserByUsernameTest() throws JSONException {
-        response = this.restTemplate.getForObject("/missionbundles/user/Rekku", String.class);
-        JSONAssert.assertEquals("{\n" +
-                "    \"username\": \"Rekku\",\n" +
-                "    \"role\": \"Testaaja\",\n" +
-                "    \"points\": 2000,\n" +
-                "    \"groupid\": 2,\n" +
-                "    \"completedmissions\": [\"Orvokki\", \"Ruiskaunokki\"],\n" +
-                "    \"contactpersonuserid\": 1\n" +
-                "}", response, false);
+        List<MissionBundle> missionBundle = responseEntity.getBody();
+        assertTrue(missionBundle.size()==2);
     }
     
     /* Call GET by id */
     @Test
-    public void getOneUserByIdTest() throws JSONException {
-        response = this.restTemplate.getForObject("/missionbundles/4", String.class);
+    public void getOneMissionBundleByIdTest() throws JSONException {
+        response = this.restTemplate.getForObject("/missionbundles/1", String.class);
         JSONAssert.assertEquals("{\n" +
-                "    \"username\": \"Rekku\",\n" +
-                "    \"role\": \"Testaaja\",\n" +
-                "    \"points\": 2000,\n" +
-                "    \"groupid\": 2,\n" +
-                "    \"completedmissions\": [\"Orvokki\", \"Ruiskaunokki\"],\n" +
-                "    \"contactpersonuserid\": 1\n" +
+                "        \"bundleid\": 1,\n" +
+                "        \"listofmissions\": [\n" +
+                "            25,\n" +
+                "            3\n" +
+                "        ],\n" +
+                "        \"bundlename\": \"Kettujutut\"\n" +
+                "    }", response, false);
+    }
+
+    /* Check if listofmissions for Jermujutut equals 1, 2 and 3 */
+    @Test
+    public void getListOfMissionsTest() throws JSONException {
+        response = this.restTemplate.getForObject("/missionbundles/2", String.class);
+        JSONAssert.assertEquals("{\n" +
+                "    1,\n" +
+                "    2,\n" +
+                "    3\n" +
                 "}", response, false);
     }
-
-    /* Check if Jermu user's completedtasks equals Himmeli and Helpperi */
-    @Test
-    public void getCompletedTasksForUserTest() throws JSONException {
-        response = this.restTemplate.getForObject("/missionbundles/auth0|5b87943afe13090f5ffd652b/completedmissions", String.class);
-        JSONAssert.assertEquals("[\"Himmeli\", \"Helpperi\"]", response, false);
-    }
-    
-    /* Check if creating a user is successful*/
-    @Test
-    public void insertUserTest() {
-        User user = new User("Kettu", "Kekkuloija", 500, 1, null, 3, "Elsa1");
-        
-        ResponseEntity<User> responseEntity = restTemplate.postForEntity(
-                urlWithPort("/missionbundles"), user, User.class);
-        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
-        System.out.println(responseEntity.getBody());
-        
-        /* Check location for creating testLocation*/
-        String testLocation = "/missionbundles/" + responseEntity.getBody().getAuthid();
-        responseEntity = restTemplate.getForEntity(testLocation, User.class);
-        assertThat(responseEntity.getStatusCodeValue(), is(200));
-        User insertedUser = responseEntity.getBody();
-        assertTrue(insertedUser.getUsername().equals(user.getUsername()));
-    }
-    
+//
+//    /* Check if creating a user is successful*/
 //    @Test
-//    public void updateMissionBundleNameTest() throws Exception {
-//        MissionBundle firstMissionBundle = getFirstMissionBundleFromDBTest();
-//        String url = urlWithPort("/missionbundles" + firstMissionBundle.getBundleid() + "/bundlename");
-//        Integer originalUsername = (Integer) Integer.valueOf(missionBundleController.getOneMissionBundleById(firstMissionBundle.getBundleid()));
-//        String updatedUsername = originalUsername + "_updated";
-//        firstMissionBundle.setUsername(updatedUsername);
-//        RequestEntity<User> requestEntity = new RequestEntity<>(firstMissionBundle, HttpMethod.PUT, new URI(url));
-//        ResponseEntity<User> responseEntity = restTemplate.exchange(requestEntity, User.class);
-//        assertEquals(updatedUsername, firstMissionBundle.getUsername());
+//    public void insertUserTest() {
+//        User user = new User("Kettu", "Kekkuloija", 500, 1, null, 3, "Elsa1");
+//
+//        ResponseEntity<User> responseEntity = restTemplate.postForEntity(
+//                urlWithPort("/missionbundles"), user, User.class);
+//        assertTrue(responseEntity.getStatusCode().is2xxSuccessful());
+//        System.out.println(responseEntity.getBody());
+//
+//        /* Check location for creating testLocation*/
+//        String testLocation = "/missionbundles/" + responseEntity.getBody().getAuthid();
+//        responseEntity = restTemplate.getForEntity(testLocation, User.class);
+//        assertThat(responseEntity.getStatusCodeValue(), is(200));
+//        User insertedUser = responseEntity.getBody();
+//        assertTrue(insertedUser.getUsername().equals(user.getUsername()));
 //    }
-
-    private MissionBundle getFirstMissionBundleFromDBTest() {
-        List<MissionBundle> allMissionBundles = missionBundleController.getAllMissionBundles();
-        return allMissionBundles.isEmpty() ? null : allMissionBundles.get(0);
-    }
-    
-    @BeforeClass
-    public static void getAuthentication() {
-        TestRestTemplate restTemplate = new TestRestTemplate();
-        String url = "https://jermu.eu.auth0.com/oauth/token";
-        AuthenticationQuery authenticationQuery = new AuthenticationQuery("password", "hirmuinen.jermu.jermu@gmail.com",
-                "Jermuilija1", "http://elsa", "etTCTSDZi6ev3eKQomKUA23YEwE0D7mw","mA1YY6aLkG2t7OsMzqjSapVBB7a1VnVJH1Zh7HuqbeUtS3mc6RPn0nQxT4mTjLJE");
-    
-        ResponseEntity<AccessToken> responseEntity = restTemplate.postForEntity(
-                url, authenticationQuery, AccessToken.class);
-        accessToken = responseEntity.getBody();
-    }
+//
+////    @Test
+////    public void updateMissionBundleNameTest() throws Exception {
+////        MissionBundle firstMissionBundle = getFirstMissionBundleFromDBTest();
+////        String url = urlWithPort("/missionbundles" + firstMissionBundle.getBundleid() + "/bundlename");
+////        Integer originalUsername = (Integer) Integer.valueOf(missionBundleController.getOneMissionBundleById(firstMissionBundle.getBundleid()));
+////        String updatedUsername = originalUsername + "_updated";
+////        firstMissionBundle.setUsername(updatedUsername);
+////        RequestEntity<User> requestEntity = new RequestEntity<>(firstMissionBundle, HttpMethod.PUT, new URI(url));
+////        ResponseEntity<User> responseEntity = restTemplate.exchange(requestEntity, User.class);
+////        assertEquals(updatedUsername, firstMissionBundle.getUsername());
+////    }
+//
+//    private MissionBundle getFirstMissionBundleFromDBTest() {
+//        List<MissionBundle> allMissionBundles = missionBundleController.getAllMissionBundles();
+//        return allMissionBundles.isEmpty() ? null : allMissionBundles.get(0);
+//    }
+//
+//    @BeforeClass
+//    public static void getAuthentication() {
+//        TestRestTemplate restTemplate = new TestRestTemplate();
+//        String url = "https://jermu.eu.auth0.com/oauth/token";
+//        AuthenticationQuery authenticationQuery = new AuthenticationQuery("password", "hirmuinen.jermu.jermu@gmail.com",
+//                "Jermuilija1", "http://elsa", "etTCTSDZi6ev3eKQomKUA23YEwE0D7mw","mA1YY6aLkG2t7OsMzqjSapVBB7a1VnVJH1Zh7HuqbeUtS3mc6RPn0nQxT4mTjLJE");
+//
+//        ResponseEntity<AccessToken> responseEntity = restTemplate.postForEntity(
+//                url, authenticationQuery, AccessToken.class);
+//        accessToken = responseEntity.getBody();
+//    }
     
 //  static boolean lippu;
 //    @Before
